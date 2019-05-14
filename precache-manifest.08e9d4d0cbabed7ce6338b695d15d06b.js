b02c725b3c10879bbf2466d4666782bca1a0cc1e
lchamberlain <lchamerlain@klipfolio.com>
2019-05-14 08:23:10 -0400

    Updates

---
+++ precache-manifest.08e9d4d0cbabed7ce6338b695d15d06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29e05806f5f1dd33a852e643d4005aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8439c3033242398d427e",
+    "url": "/static/css/main.aca3ae5b.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8439c3033242398d427e",
+    "url": "/static/js/main.04f8ffe7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7797f6229b01fd22c7f0d6a5410db009",
+    "url": "/static/media/munro.7797f622.ttf"
+  }
+]);
